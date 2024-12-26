21370bc5f9f93175c8d2857456ce9bcb8cd588e5
Jon Spalding <jon.spalding@googlemail.com>
2024-12-26 12:46:40 +0000

    Push files to printer rather than use update manager

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-cp -r ./files/* ~/printer_data/config/
+set -e
+
+# backup the current configuration - ssh in and zip up directory
+ssh pi@batrig.local "cd ~/printer_data/ && zip -r --exclude=*.git* printer-data-backup-\$(date +%Y%m%d%H%M%S).zip config/"
+
+# copy the files to the printer
+scp -r ./files/* pi@batrig.local:~/printer_data/config/
