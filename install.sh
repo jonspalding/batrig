83c2003f1afc8fe0bbc5732da1c6e8e84bf5a41c
Jon Spalding <jon.spalding@googlemail.com>
2025-02-22 12:29:07 +0000

    Toolchanger baseline working

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 
 # backup the current configuration - ssh in and zip up directory
 ssh pi@batrig.local "cd ~/printer_data/ && zip -r --exclude=*.git* printer-data-backup-\$(date +%Y%m%d%H%M%S).zip config/"
+# ssh pi@batrig.local "cd ~/klipper/klippy/ && zip -r --exclude=*.git* printer-klippy-extras-backup-\$(date +%Y%m%d%H%M%S).zip extras/"
 
 # copy the files to the printer
-scp -r ./files/* pi@batrig.local:~/printer_data/config/
+scp -r ./config/* pi@batrig.local:~/printer_data/config/
+# scp -r ./klippy-extras/* pi@batrig.local:~/klipper/klippy/extras
